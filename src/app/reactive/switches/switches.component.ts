4868e542b602bb86f5e390b4789eba0598072cbe
amilcar <52044033+Terminal-Redux@users.noreply.github.com>
2022-05-29 08:27:13 -0500

    Fin de seccion 17

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue],
+  });
 
-  constructor() { }
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.miFormulario.reset({ ...this.persona, condiciones: false });
+
+    this.miFormulario.valueChanges.subscribe((form) => {
+      const formNoRef = { ...form };
+      delete formNoRef.condiciones;
+      this.persona = formNoRef;
+    });
   }
 
+  guardar() {
+    const formValue = { ...this.miFormulario.value };
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+  }
 }
